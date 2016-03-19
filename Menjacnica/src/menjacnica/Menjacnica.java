e5da9372b98674a3e3f36d708c2b0c91e2183d63
Milos Cvetkovic <miloscv@gmail.com>
2016-03-19 14:20:36 +0100

    Do kraja implementirana klasa Menjacnica.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,6 +1,7 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 import valute.Valuta;
@@ -8,23 +9,66 @@ import valute.kursevi.Kurs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> valute; // Valute koje koristi Menjacnica
+
+	public Menjacnica(LinkedList<Valuta> valute) {
+		this.valute = valute;
+	}
+
 	@Override
 	public void dodavanjeKursaValuteZaOdredjeniDan(Valuta valuta, GregorianCalendar datum, double prodajni,
 			double kupovni, double srednji) {
-		// TODO Auto-generated method stub
+
+		Kurs k = new Kurs();
+		k.setDatum(datum);
+		k.setProdajni(prodajni);
+		k.setKupovni(kupovni);
+		k.setSrednji(srednji);
+
+		for (Valuta i : valute) {
+			if (i.getKurs().contains(k)) {
+				throw new RuntimeException("Vec postoji kurs za taj dan.");
+			} else if (i.equals(valuta)) {
+				i.setKurs(k);
+				return;
+			}
+		}
 
 	}
 
 	@Override
 	public void brisanjeKursaValuteZaOdredjeniDan(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+
+		for (Valuta i : valute) {
+			if (i.equals(valuta)) {
+				for (int j = 0; j < i.getKurs().size(); j++) {
+					if (i.getKurs().get(j).getDatum().equals(datum)) {
+						i.getKurs().remove(j);
+						return;
+					}
+				}
+			}
+		}
+
+		throw new RuntimeException("Ne postoji kurs za taj datum.");
 
 	}
 
 	@Override
 	public Kurs vracanjeKursaValuteZaOdredjeniDan(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return null;
+
+		for (Valuta i : valute) {
+			if (i.equals(valuta)) {
+				for (Kurs j : i.getKurs()) {
+					if (j.getDatum().equals(datum)) {
+						return j;
+					}
+				}
+			}
+		}
+
+		throw new RuntimeException("Ne postoji kurs za taj datum.");
+
 	}
 
 }
