6edb7c0049ea3aa296a1513627f78fb65910a79d
Milos Cvetkovic <miloscv@gmail.com>
2016-03-19 14:05:05 +0100

    Redefinisane metode equals, hashCode i toString u klasama Valute i Kurs.

--- Menjacnica/src/valute/Valuta.java
+++ Menjacnica/src/valute/Valuta.java
@@ -34,4 +34,34 @@ public class Valuta {
 		this.kurs = kurs;
 	}
 
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", nazivSkr=" + nazivSkr + ", kurs=" + kurs + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nazivSkr == null) ? 0 : nazivSkr.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (nazivSkr == null) {
+			if (other.nazivSkr != null)
+				return false;
+		} else if (!nazivSkr.equals(other.nazivSkr))
+			return false;
+		return true;
+	}
+
 }
--- Menjacnica/src/valute/kursevi/Kurs.java
+++ Menjacnica/src/valute/kursevi/Kurs.java
@@ -41,4 +41,35 @@ public class Kurs {
 		this.srednji = srednji;
 	}
 
+	@Override
+	public String toString() {
+		return "Kurs [datum=" + datum + ", prodajni=" + prodajni + ", kupovni=" + kupovni + ", srednji=" + srednji
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		return true;
+	}
+
 }
